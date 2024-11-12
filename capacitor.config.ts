96cef0162b20ed67c6e10ecf3e6de4b5938d3946
Marco Sa <marco.sa@md3.pt>
2024-11-12 22:00:26 +0000

    add ios platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'segmentView',
+  webDir: 'www'
+};
+
+export default config;
